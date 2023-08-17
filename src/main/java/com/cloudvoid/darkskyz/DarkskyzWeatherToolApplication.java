c1f4ac4de3d52aceca1825025b6fbfbb2f0acf9d
Ilucx <d.juraske@googlemail.com>
2023-08-17 13:07:30 +0200

    project init

---
+++ src/main/java/com/cloudvoid/darkskyz/DarkskyzWeatherToolApplication.java
@@ -0,0 +1,13 @@
+package com.cloudvoid.darkskyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkskyzWeatherToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkskyzWeatherToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudvoid/darkskyz/DarkskyzWeatherToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudvoid.darkskyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkskyzWeatherToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
